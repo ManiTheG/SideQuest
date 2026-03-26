e934f987317cbdfe0afd8cde8ee711d3e27f889b
LukatoniCode <lukatonicode@gmail.com>
2026-03-26 23:51:50 +0100

    homescreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,8 +11,49 @@ class _HomeScreenState extends State<HomeScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-      body: const Center(child: Text("Welcome to SideQuest!")),
+      backgroundColor: Color.fromARGB(255, 16, 24, 41),
+      body: SafeArea(
+        top: false,
+        child: Column(
+        children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 30, 41, 57),
+                border: Border(bottom: BorderSide(
+                  color: Colors.blueGrey,
+                  width: 0.5
+                ))
+              ),
+              child: Padding(
+                padding: EdgeInsets.only(top: 50, left: 16, right: 16, bottom: 12),
+                child: Container(
+                  margin: EdgeInsets.all(5.0),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 55, 73, 87),
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 12,
+                        horizontal: 12,
+                      ),
+                      hintText: "Search Posts",
+                      hintStyle: TextStyle(color: Colors.grey),
+                      suffixIcon: Icon(Icons.search, color: Colors.grey),
+                      border: InputBorder.none,
+                    ),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+            // search bar goes here
+            // category pills go here
+            // post list goes here
+          ],
+      ))
     );
   }
 
