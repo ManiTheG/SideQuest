bc80e54d807aae44111d908f76a1b4b25546fc23
ManiTheG <emanuel1emanuel@gmail.com>
2026-03-27 15:00:06 +0100

    Toni dizajniraj plz

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../services/auth_service.dart';
 
 class SignupScreen extends StatefulWidget{
   const SignupScreen({super.key});
@@ -17,9 +20,42 @@ class _SignupScreenState extends State<SignupScreen>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
- 
-      appBar: AppBar(title: const Text("Signup")),
-      body: const Center(child: Text("Welcome to the Signup Screen!")),
+      //resizeToAvoidBottomInset: true,
+      appBar: AppBar(title: const Text('Sign up')),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+
+            //koisnicko ime
+            TextField(
+              controller: _userNameController,
+              decoration: const InputDecoration(labelText: 'Username'),
+            ),
+            const SizedBox(height: 16),
+            //email
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress
+            ),
+            const SizedBox(height: 16),
+            //loinka/zaporka
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+
+            //lista hobbija
+            const Text('Select your interests:',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
