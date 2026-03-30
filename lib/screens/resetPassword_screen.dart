7a1a75f86ff3fb5823930c29551fcdd01a9025c2
ManiTheG <emanuel1emanuel@gmail.com>
2026-03-30 22:09:07 +0200

    resetPassword strelicu na to left sve ostalo na sredinu

--- lib/screens/resetPassword_screen.dart
+++ lib/screens/resetPassword_screen.dart
@@ -12,17 +12,30 @@ class ResetPasswordScreen extends StatefulWidget {
 
 class _ResetPasswordScreenState extends State<ResetPasswordScreen>{
   final _emailController  = TextEditingController();
-  final AuthService _auth =AuthService();
-  bool isLoading = false;
+  final AuthService _authService = AuthService();
+  bool _isLoading = false;
+  bool _isShown = true;
   String? _errorMessage;
 
   @override
-  void dipose(){
+  void dispose(){
     _emailController.dispose();
     super.dispose();
   }
 
-
+  Future <void> _resetPassword() async{
+    if(_emailController.text.isEmpty){
+      setState(() => _errorMessage = 'Please enter your email to reset password.');
+      _emailController.clear();
+      return;
+    }
+    try{
+      await _authService.resetPassword(_emailController.text);
+      setState(()=> _errorMessage = 'Password reset email sent. Please check your inbox.');
+    } catch(e){
+      setState(() => _errorMessage = e.toString());
+    }
+  }
 
 
   @override
@@ -38,11 +51,71 @@ class _ResetPasswordScreenState extends State<ResetPasswordScreen>{
           ),
         ),
 
-        child: Padding(
+        child: _isShown ? Padding(
         padding: const EdgeInsets.all(24),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
 
+            SafeArea(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 16, 103, 234),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.arrow_back, color: Colors.white),
+                      onPressed: () => Navigator.pop(context),
+                      padding: EdgeInsets.all(8),
+                      constraints: BoxConstraints(),
+                    ),
+                  ),
+                ),
+              ),
+
+
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                labelStyle: TextStyle(color: Colors.white60),
+                filled: true,
+                fillColor: Color.fromARGB(255, 55, 73, 87),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+
+            _isLoading? const Center(child: CircularProgressIndicator())
+            //gumb za poziv login funkcije
+            : ElevatedButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                alignment: Alignment.center,
+              ),
+              onPressed: (){
+                 setState(() { _isShown = !_isShown;});
+                 _resetPassword();
+                 },
+               child: const Text('Send password reset email.')),
+
+          ],
         ),
+        ):
+        Center(
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            
+          ],
+          ),
         ),
       ),
     );
